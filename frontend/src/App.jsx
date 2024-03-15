2189a7894272a27b731fe9b04a3622affe6949e1
dineshs25 <dineshs25201@gmail.com>
2024-03-15 18:39:53 +0530

    15-03-2024-completed-blogs-products-subcategory and merged

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 // // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home'
 import About from './pages/About'
@@ -32,43 +32,43 @@ import SiteConfig from './pages/admin/configuration/SiteConfig';
 import SiteConfigBanner from './components/siteconfig/Banner';
 import SiteConfigAbout from './components/siteconfig/About.jsx';
 
-function App () {
-  return(
+function App() {
+  return (
     <Router>
-   
-              <Routes>
-                  <Route path="*" element={<div></div>} />
-                  <Route exact path="/admin" element={<Dashboard/>} />
-                  <Route exact path="/products" element={<ProductList/>} />
-                  <Route exact path="/add-product" element={<AddProduct/>} />
-                  <Route exact path="/products/:edit" element={<EditProduct/>} />
-                  <Route exact path="/category" element={<Category/>} />
-                  <Route exact path="/catering" element={<Catering/>} />
-                  <Route exact path="/admin-blogs" element={<BlogList/>} />
-                  <Route exact path="/add-blogs" element={<AddBlog/>} />
-                  <Route exact path="/admin-blogs/:edit" element={<EditBlog/>} />
-                  <Route exact path="/orders" element={< Orders/>} />
-                  <Route exact path="/site-config" element={< SiteConfig/>} />
-                  <Route exact path="/site-config/banner" element={< SiteConfigBanner/>} />
-                  <Route exact path="/site-config/about" element={< SiteConfigAbout/>} />
-                  <Route exact path="/locations" element={<div>LOcations</div>} />
-                  <Route exact path="/profile" element={<div>Profile</div>} />
-                  <Route exact path="/login" element={<Login/>}/> 
-                  <Route exact path="/user-login" element={<UserLogin/>}/> 
-                  <Route exact path="/user-register" element={<UserRegister/>}/> 
-                  <Route exact path="/forgot-password" element={<ForgotPassword/>}/> 
 
-                  <Route exact path='/' element={<Home />}/>
-                  <Route exact path='/about' element={<About />}/>
-                  <Route exact path='/menu' element={<Menu/>}/>
-                  <Route exact path='/book-table' element={<BookTable />}/>
-                  <Route exact path='/blog' element={<Blog />}/>
-                  <Route exact path='/cart' element={<Cart/>}/>
-                  <Route exact path='/wishlist' element={<Wishlist/>}/>
-                  <Route exact path='/menu/:id' element={<ProductDetails/>}/>
-                  <Route exact path='/blog/:slug' element={<BlogDetails />}/>
-                  <Route exact path='/contact-us' element={<Contact />}/>
-          </Routes>
+      <Routes>
+        <Route path="*" element={<div></div>} />
+        <Route exact path="/admin" element={<Dashboard />} />
+        <Route exact path="/products" element={<ProductList />} />
+        <Route exact path="/add-product" element={<AddProduct />} />
+        <Route exact path="/products/:edit" element={<EditProduct />} />
+        <Route exact path="/category" element={<Category />} />
+        <Route exact path="/catering" element={<Catering />} />
+        <Route exact path="/admin-blogs" element={<BlogList />} />
+        <Route exact path="/add-blogs" element={<AddBlog />} />
+        <Route exact path="/admin-blogs/:edit" element={<EditBlog />} />
+        <Route exact path="/orders" element={< Orders />} />
+        <Route exact path="/site-config" element={< SiteConfig />} />
+        <Route exact path="/site-config/banner" element={< SiteConfigBanner />} />
+        <Route exact path="/site-config/about" element={< SiteConfigAbout />} />
+        <Route exact path="/locations" element={<div>LOcations</div>} />
+        <Route exact path="/profile" element={<div>Profile</div>} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/user-login" element={<UserLogin />} />
+        <Route exact path="/user-register" element={<UserRegister />} />
+        <Route exact path="/forgot-password" element={<ForgotPassword />} />
+
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/menu' element={<Menu />} />
+        <Route exact path='/book-table' element={<BookTable />} />
+        <Route exact path='/blog' element={<Blog />} />
+        <Route exact path='/cart' element={<Cart />} />
+        <Route exact path='/wishlist' element={<Wishlist />} />
+        <Route exact path='/menu/:id' element={<ProductDetails />} />
+        <Route exact path='/blog/:slug' element={<BlogDetails />} />
+        <Route exact path='/contact-us' element={<Contact />} />
+      </Routes>
     </Router>
   )
 }
